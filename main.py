d93bf0eaea78670e610b4bdf6b8b0737246d01c8
sexnthefloor <vadimka_rasskazov@mail.ru>
2023-06-07 18:01:14 +0300

    Вывод нового списка

--- main.py
+++ main.py
@@ -3,3 +3,4 @@
 # помещая в него сначала все отрицательные, затем нулевые и, нако¬нец, положительные элементы.
 first_list = [5, -2, 3, 0, 2, 0, -9, -11, 4, 9, 1, -5, 0]
 second_list = [i for i in first_list if i<0] + [i for i in first_list if i==0] + [i for i in first_list if i>0]
+print (second_list)
